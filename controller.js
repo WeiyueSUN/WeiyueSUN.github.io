c38688e6178e5b60f564fff58cd55c2abc7cdb47
WeiyueSUN <sunweiyue0203@gmail.com>
2017-05-10 14:38:43 +0800

    0.2

--- controller.js
+++ controller.js
@@ -1,61 +1,78 @@
 $(document).ready(function () {
 
-var questionNumber=0;
-var questionBank=new Array();
+var qNumber=0;
+var qbank=new Array();
+var opArray=new Array();
+var myloader="activity.json";
 var stage="#game1";
 var stage2=new Object;
-var questionLock=false;
+var qlock=false;
 var numberOfQuestions;
 var score=0;
 
+fillDB();
 
 
 
-$.getJSON('activity.json', function(data) {
-
-for(i=0;i<data.quizlist.length;i++){
-			questionBank[i]=new Array;
-			questionBank[i][0]=data.quizlist[i].question;
-			questionBank[i][1]=data.quizlist[i].option1;
-			questionBank[i][2]=data.quizlist[i].option2;
-			questionBank[i][3]=data.quizlist[i].option3;
-}
-numberOfQuestions=questionBank.length;
-//alert(questionBank);
+function fillDB(){
 
 
-displayQuestion();
-})//gtjson
+
+ 		$.getJSON(myloader, function(data) {
 
 
+		for(i=0;i<data.quizlist.length;i++){
+
+			qbank[i]=new Array;
+			qbank[i][0]=data.quizlist[i].question;
+			qbank[i][1]=data.quizlist[i].op1;
+			qbank[i][2]=data.quizlist[i].op2;
+			qbank[i][3]=data.quizlist[i].op3;
+
+		}
+		 numberOfQuestions=qbank.length;
+
+		 //alert(qbank);
+
+		 displayQuestion();
+
+
+		})//gtjson
+
+}//filldb
 
 
 function displayQuestion(){
- var rnd=Math.random()*3;
-rnd=Math.ceil(rnd);
- var q1;
- var q2;
- var q3;
 
-if(rnd==1){q1=questionBank[questionNumber][1];q2=questionBank[questionNumber][2];q3=questionBank[questionNumber][3];}
-if(rnd==2){q2=questionBank[questionNumber][1];q3=questionBank[questionNumber][2];q1=questionBank[questionNumber][3];}
-if(rnd==3){q3=questionBank[questionNumber][1];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][3];}
+var rnd=Math.random()*3;rnd=Math.ceil(rnd);
+var q1;
+var q2;
+var q3;
+if(rnd==1){q1=qbank[qNumber][1];q2=qbank[qNumber][2];q3=qbank[qNumber][3];}
+if(rnd==2){q2=qbank[qNumber][1];q3=qbank[qNumber][2];q1=qbank[qNumber][3];}
+if(rnd==3){q3=qbank[qNumber][1];q1=qbank[qNumber][2];q2=qbank[qNumber][3];}
 
-$(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</div><div id="1" class="option">'+q1+'</div><div id="2" class="option">'+q2+'</div><div id="3" class="option">'+q3+'</div>');
+$(stage).append('<div class="questionText">'+qbank[qNumber][0]+'</div><div id="1" class="pix"><img src="img/'+q1+'"></div><div id="2" class="pix"><img src="img/'+q2+'"></div><div id="3" class="pix"><img src="img/'+q3+'"></div>');
 
-$('.option').click(function(){
-  if(questionLock==false){questionLock=true;
-  //correct answer
-  if(this.id==rnd){
-   $(stage).append('<div class="feedback1">CORRECT</div>');
-   score++;
-   }
-  //wrong answer
-  if(this.id!=rnd){
-   $(stage).append('<div class="feedback2">WRONG</div>');
-  }
-  setTimeout(function(){changeQuestion()},1000);
- }})
+$('.pix').click(function(){
+			if(qlock==false){qlock=true;
+			//correct answer
+			if(this.id==rnd){
+				$(stage).append('<div class="feedback1">CORRECT</div>');
+				 score++;
+				}
+			//wrong answer
+			if(this.id!=rnd){
+				$(stage).append('<div class="feedback2">WRONG</div>');
+
+
+				}
+
+			setTimeout(function(){changeQuestion()},1000);
+
+			}})
+
+
 
 }//display question
 
@@ -63,34 +80,27 @@ $('.option').click(function(){
 
 
 
+	function changeQuestion(){
 
-function changeQuestion(){
+		qNumber++;
 
-questionNumber++;
+	if(stage=="#game1"){stage2="#game1";stage="#game2";}
+		else{stage2="#game2";stage="#game1";}
 
-if(stage=="#game1")
-	{stage2="#game1";stage="#game2";}
-else
-	{stage2="#game2";stage="#game1";}
+	if(qNumber<numberOfQuestions){displayQuestion();}else{displayFinalSlide();}
 
-if(questionNumber<numberOfQuestions)
-	{displayQuestion();}
-else
-	{displayFinalSlide();}
-
-$(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
-$(stage).animate({"right": "+=800px"},"slow", function() {questionLock=false;});
-
-}//change question
+	 $(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
+	 $(stage).animate({"right": "+=800px"},"slow", function() {qlock=false;});
+	}//change question
 
 
 
 
-function displayFinalSlide(){
+	function displayFinalSlide(){
 
-$(stage).append('<div class="questionText">You have finished the quiz!<br><br>Total questions: '+numberOfQuestions+'<br>Correct answers: '+score+'</div>');
+		$(stage).append('<div class="questionText">You have finished the quiz!<br><br>Total questions: '+numberOfQuestions+'<br>Correct answers: '+score+'</div>');
 
-}//display final slide
+	}//display final slide
 
 
 

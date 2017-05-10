8a4ea2668773d98b3c11ec6f5c820ed3e81c6757
Aaron Xia <aaronxia@foxmail.com>
2017-05-10 22:04:10 +0800

    bug fixed

--- controller.js
+++ controller.js
@@ -1,97 +1,99 @@
 $(document).ready(function () {
 
-var qNumber=0;
-var qbank=new Array();
-var opArray=new Array();
-var myloader="activity.json";
+	var qNumber=0;
+	var qbank=new Array();
+	var opArray=new Array();
+	var myloader="activity.json";
 //var myloader = "https://github.com/WeiyueSUN/WeiyueSUN.github.io/blob/master/activity.json";
-var stage="#game1";
-var stage2=new Object;
-var qlock=false;
-var numberOfQuestions;
-var score=0;
+	var stage="#game1";
+	var stage2=new Object;
+	var qlock=false;
+	var numberOfQuestions;
+	var score=0;
 
-function fillDB(){
+	function fillDB(){
 
 
 
- 		$.getJSON(myloader, function(data) {
+		$.getJSON(myloader, function(data) {
 
-		console.log(data);
-		for(i=0;i<data.quizlist.length;i++){
+			//console.log(data);
+			for(i=0;i<data.quizlist.length;i++){
 
-			qbank[i]=new Array;
-			qbank[i][0]=data.quizlist[i].question;
-			qbank[i][1]=data.quizlist[i].option1;
-			qbank[i][2]=data.quizlist[i].option2;
-			qbank[i][3]=data.quizlist[i].option3;
-			qbank[i][4]=data.quizlist[i].option4;
+				qbank[i]=new Array;
+				qbank[i][0]=data.quizlist[i].question;
+				qbank[i][1]=data.quizlist[i].option1;
+				qbank[i][2]=data.quizlist[i].option2;
+				qbank[i][3]=data.quizlist[i].option3;
+				qbank[i][4]=data.quizlist[i].option4;
 
-		}
-		 numberOfQuestions=qbank.length;
-		 console.log(qbank);
-		 //alert(qbank);
+			}
+			numberOfQuestions=qbank.length;
+			console.log(qbank);
+			//alert(qbank);
 
-		 displayQuestion();
+			displayQuestion();
 
 
 		})//gtjson
 
-}//filldb
+	}//filldb
 
 
 
 
 
 
-function generateRndSeq(n){
+	function generateRndSeq(n){
 
-	var A = new Array;
-	var rnd;
-	for (var i = 0; i < n; i++){
-		A[i] = i + 1;
+		var A = new Array;
+		var rnd;
+		for (var i = 0; i < n; i++){
+			A[i] = i + 1;
+		}
+		for (var i = 0; i < n - 1; i++) {
+			rnd = Math.random() * (n - i);
+			rnd = Math.floor(rnd);
+			temp = A[i];
+			A[i] = A[rnd+i];
+			A[rnd+i] = temp;
+		}
+		return A;
 	}
-	for (var i = 0; i < n - 1; i++) {
-		rnd = Math.random() * (n - i);
-		rnd = Math.floor(rnd);
-		[A[i], A[rnd + i]] = [A[rnd + i], A[i]];
-	}
-	return A;
-}
 
-function displayQuestion(){
+	function displayQuestion(){
 
-var seq = generateRndSeq(4);
-console.log(q);
-var ansid;
-var s = '<div class="questionText">' + qbank[qNumber][0] + '</div>';
-for (var i = 0; i < 4; i++) {
-	if (seq[i] == 1) {ansid = i;}
-	s = s + '<div id="' + i + '" class="pix"><img src="img/' + qbank[qNumber][seq[i]] + '"></div>';
-}
-console.log(s);
-$(stage).append(s);
-$('.pix').click(function(){
+		var seq = generateRndSeq(4);
+		//console.log(q);
+		var ansid;
+		var s = '<div class="questionText">' + qbank[qNumber][0] + '</div>';
+		for (var i = 0; i < 4; i++) {
+			if (seq[i] == 1) {ansid = i;}
+			s = s + '<div id="' + i + '" class="pix"><img src="img/' + qbank[qNumber][seq[i]] + '"></div>';
+		}
+		//console.log(s);
+		$(stage).append(s);
+		$('.pix').click(function(){
 			if(qlock==false){qlock=true;
-			//correct answer
-			if(this.id==ansid){
-				$(stage).append('<div class="feedback1">CORRECT</div>');
-				 score++;
+				//correct answer
+				if(this.id==ansid){
+					$(stage).append('<div class="feedback1">CORRECT</div>');
+					score++;
 				}
-			//wrong answer
-			if(this.id!=ansid){
-				$(stage).append('<div class="feedback2">WRONG</div>');
+				//wrong answer
+				if(this.id!=ansid){
+					$(stage).append('<div class="feedback2">WRONG</div>');
 
 
 				}
 
-			setTimeout(function(){changeQuestion()},1000);
+				setTimeout(function(){changeQuestion()},1000);
 
 			}})
 
 
 
-}//display question
+	}//display question
 
 
 
@@ -101,13 +103,13 @@ $('.pix').click(function(){
 
 		qNumber++;
 
-	if(stage=="#game1"){stage2="#game1";stage="#game2";}
+		if(stage=="#game1"){stage2="#game1";stage="#game2";}
 		else{stage2="#game2";stage="#game1";}
 
-	if(qNumber<numberOfQuestions){displayQuestion();}else{displayFinalSlide();}
+		if(qNumber<numberOfQuestions){displayQuestion();}else{displayFinalSlide();}
 
-	 $(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
-	 $(stage).animate({"right": "+=800px"},"slow", function() {qlock=false;});
+		$(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
+		$(stage).animate({"right": "+=800px"},"slow", function() {qlock=false;});
 	}//change question
 
 
@@ -120,9 +122,9 @@ $('.pix').click(function(){
 	}//display final slide
 
 
-fillDB();
+	fillDB();
 
 
 
 
-	});//doc ready
+});//doc ready
